d5c23924f5d28bbe8e2b6da1a7ce557f19a80c2b
antonio.siqueira <antonio.siqueira@basis.com.br>
2020-05-17 14:50:24 -0300

    Criando FormGroup para Controlar o Formulário

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,22 +1,82 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @IonicPage()
 @Component({
-  selector: 'page-signup',
-  templateUrl: 'signup.html',
+	selector: 'page-signup',
+	templateUrl: 'signup.html',
 })
 export class SignupPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+	formGroup: FormGroup;
 
-  signupUser() {
-    console.log("Enviou o Form");
-  }
+	constructor(
+		public navCtrl: NavController, 
+		public navParams: NavParams,
+		public formBuilder: FormBuilder
+		) 
+	{ 
+		this.formGroup = this.formBuilder.group({
+			nome: [
+				'Teste', 
+				[
+					Validators.required, 
+					Validators.minLength(5), 
+					Validators.maxLength(120)
+				]
+			],
 
-  //ionViewDidLoad() {
-  //  console.log('ionViewDidLoad SignupPage');
-  //}
+			email: [
+				'mail@mail.com',
+				[
+					Validators.required,
+					Validators.email
+				]
+			],
+
+			tipo: ['1', [Validators.required]],
+
+			cpfOuCnpj: [
+				'61989762115', 
+				[
+					Validators.required,
+					Validators.minLength(11),
+					Validators.maxLength(14)
+				]
+			],
+
+			senha: ['123', [Validators.required]],
+
+			logradouro: ['Rua', [Validators.required]],
+
+			numero: ['10', [Validators.required]],
+
+			complemento:['Apto Xpto', []],
+
+			bairro: ['Guará II', []],
+
+			cep: ['71070000', [Validators.required]],
+
+			telefone1: ['6199998525', [Validators.required]],
+
+			telefone2: ['', []],
+
+			telefone3: ['', []],
+
+			estadoId: [null, [Validators.required]],
+
+			cidadeId: [null, [Validators.required]]
+
+		});
+	}
+
+	signupUser() {
+		console.log("Enviou o Form");
+	}
+
+	//ionViewDidLoad() {
+	//  console.log('ionViewDidLoad SignupPage');
+	//}
 
 }
